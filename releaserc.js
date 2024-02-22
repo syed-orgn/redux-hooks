979dbd0cc86c6b62eb520cc36529f3657b5dca19
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-22 14:21:37 +0530

    chore: Removed RepoUrl

--- releaserc.js
+++ releaserc.js
@@ -1,6 +1,5 @@
 module.exports = {
   branches: ["master"], // Only triggers on pushes to master branch
-  repositoryUrl: "https://github.com/syed-orgn/redux-hooks",
   plugins: [
     ["@semantic-release/commit-analyzer", {
       preset: "angular", // Customize commit message analysis
