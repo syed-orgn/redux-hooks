0a1dcd1305dbed7f6dc87f3e5b025bce8ca4acd1
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-22 16:31:41 +0530

    feat: Major Changes

--- releaserc.js
+++ releaserc.js
@@ -4,9 +4,5 @@ module.exports = {
     ["@semantic-release/commit-analyzer", {
       preset: "angular", // Customize commit message analysis
     }],
-    ["@semantic-release/release-notes-generator", {
-      generator: "angular", // Use Angular-style release notes
-      writer: "conventional", // Output format for release notes
-    }],
   ],
 };
