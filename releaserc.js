d9cd6ad701d4bf2a6556d60090cefc826d9132ed
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-22 18:04:39 +0530

    feat: releaserc.js code commented

--- releaserc.js
+++ releaserc.js
@@ -1,13 +1,13 @@
-module.exports = {
-  branches: ["master"], // Only triggers on pushes to master branch
-  plugins: [
-    ["@semantic-release/commit-analyzer", {
-      preset: "angular", // Customize commit message analysis
-    }],
-    ["@semantic-release/release-notes-generator", {
-      generator: "angular", // Use Angular-style release notes
-      writer: "conventional", // Output format for release notes
-      commit: 'commits'
-    }],
-  ],
-};
+// module.exports = {
+//   branches: ["master"], // Only triggers on pushes to master branch
+//   plugins: [
+//     ["@semantic-release/commit-analyzer", {
+//       preset: "angular", // Customize commit message analysis
+//     }],
+//     ["@semantic-release/release-notes-generator", {
+//       generator: "angular", // Use Angular-style release notes
+//       writer: "conventional", // Output format for release notes
+//       commit: 'commits'
+//     }],
+//   ],
+// };
