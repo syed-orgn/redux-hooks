fb00e4b2da1c2ed408f7cc1284a3095bbd64b28e
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-26 14:47:18 +0530

    feat: New Feature Added

--- src/send-email/index.js
+++ src/send-email/index.js
@@ -18,6 +18,8 @@ console.log("Added another console")
 // Major changes in Index file
 // Major changes in Index file
 // Major changes in Index file
+// Major changes in Index file
+// Major changes in Index file
 
 // Major changes in Index fileminorminorminminoror
 // Major changes in Index fileminor
