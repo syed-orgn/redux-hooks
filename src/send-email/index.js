651d5d6706252db2c9cd0065aad5c475c9aa97ea
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-26 13:35:52 +0530

    bug: Bug Fixed

--- src/send-email/index.js
+++ src/send-email/index.js
@@ -27,6 +27,9 @@ console.log("Added another console")
 // Major changes in Index file
 // Major changes in Index file
 // Major changes in Index file
+// Major changes in Index file
+// Major changes in Index file
+// Major changes in Index file
 // feature added
 // feature added
 // feature added
