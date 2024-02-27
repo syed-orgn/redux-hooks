cefd7f2547d9c26f8a1cbe21249a41569cd2d66c
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-27 18:29:25 +0530

    patch changes

--- src/send-email/index.js
+++ src/send-email/index.js
@@ -73,6 +73,7 @@ console.log("Added another console")
 // Major changes in Index file
 // Major changes in Index file
 // Major changes in Index file
+// Major changes in Index file
 // feature added
 // feature added
 // feature added
