2340b90e9ca3fd44c3ea6e48595988a067ea20f3
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-27 18:09:13 +0530

    Index File Changed

--- src/send-email/index.js
+++ src/send-email/index.js
@@ -47,6 +47,9 @@ console.log("Added another console")
 // Major changes in Index fileminorminorminminoror
 // Major changes in Index fileminorminorminminoror
 // Major changes in Index fileminorminorminminoror
+// Major changes in Index fileminorminorminminoror
+// Major changes in Index fileminorminorminminoror
+// Major changes in Index fileminorminorminminoror
 // Major changes in Index fileminor
 // Major changes in Index file
 // Major changes in Index file
