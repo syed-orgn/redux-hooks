8d9222c8b51c9d02721a064bd87ca419aec46f53
abbas-cedcommerce <Abbas-CedCommerce>
2024-02-27 18:24:30 +0530

    Changes Inin

--- src/send-email/index.js
+++ src/send-email/index.js
@@ -67,6 +67,8 @@ console.log("Added another console")
 // Major changes in Index file
 // Major changes in Index file
 // Major changes in Index file
+// Major changes in Index file
+// Major changes in Index file
 // feature added
 // feature added
 // feature added
